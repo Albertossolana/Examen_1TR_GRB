2615c0c8d8b7069b9c606429f96e4a249fa05a03
Albertossolana <72734513+Albertossolana@users.noreply.github.com>
2020-12-09 20:50:03 +0100

    MovCamara
    Le sigue en el eje vertical y con la mirada en el horizontal.

--- SanchezSolana_Alberto/Assets/Scripts/Camara.cs
+++ SanchezSolana_Alberto/Assets/Scripts/Camara.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Camara : MonoBehaviour
 {
     [SerializeField] Transform Objetivo;
-  
+    [SerializeField] Transform InitPos;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +15,7 @@ public class Camara : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(transform.position.x, Objetivo.position.y + 65, Objetivo.position.z);
-       
+        transform.position = new Vector3(transform.position.x, Objetivo.position.y + 100, transform.position.z);
+        transform.LookAt(Objetivo);
     }
 }
