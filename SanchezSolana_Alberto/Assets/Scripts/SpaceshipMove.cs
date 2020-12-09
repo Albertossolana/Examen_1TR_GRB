63fd3e1088769a51e7facbb1b1346c33edcbf382
Albertossolana <72734513+Albertossolana@users.noreply.github.com>
2020-12-09 18:44:52 +0100

    Movimiento Nave
    He conseguido aplicarle el movimiento horizontal y que las teclas del mando puedan hacer que suba y baje la nave.

---
+++ SanchezSolana_Alberto/Assets/Scripts/SpaceshipMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipMove : MonoBehaviour
+{
+    [SerializeField] float speed = 1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovNave();
+
+
+    }
+
+    void MovNave()
+    {
+        float MovHorizontal = Input.GetAxis("Horizontal") * speed;
+        if (MovHorizontal < -1)
+        {
+            transform.Translate(Vector3.left * -MovHorizontal * Time.deltaTime);
+        }
+        if (Input.GetButton("Up"))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+           
+        }
+        if (Input.GetButton("Down"))
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * speed);
+        }
+        if (Input.GetButton("Delante"))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+    }
+}
